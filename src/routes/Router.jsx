447015a2a40de40d2e0e3e6cc33141a95cb002e7
HoangAnh <hoanganh51199@gmail.com>
2023-10-27 21:13:05 +0700

    admin lan 2

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,9 +3,20 @@ import { useRoutes } from "react-router-dom";
 import AdminLaysOut from "../layouts/AdminLaysOut/AdminLaysOut";
 import LoginLayout from "../layouts/LoginLayout/LoginLayout";
 import AdminUser from "../pages/Admin/AdminUser/AdminUser";
+// import Login from "../components/Login/Login";
 
 export default function Router() {
   const routing = useRoutes([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        }
+      ]
+    },
     {
       path: "/admin",
       element: <AdminLaysOut/>,
@@ -20,6 +31,13 @@ export default function Router() {
     {
       path: "/form",
       element: <LoginLayout/>,
+      // children: [
+      //   {
+      //     path: "/form/login",
+      //     element: <Login />,
+      //   },
+       
+      // ],
    
     },
   ]);
