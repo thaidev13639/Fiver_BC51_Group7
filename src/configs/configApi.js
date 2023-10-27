5ebcf3868853d877a4e0fab060b1d6c3ae3acc83
HoangAnh <hoanganh51199@gmail.com>
2023-10-28 02:56:36 +0700

    login

---
+++ src/configs/configApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { BASE_URL, TOKEN_CYBERSOFT } from "../constants/apiContants";
+import { store } from "../redux-toolkit/configStore";
+
+
+const requestApi = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    TokenCybersoft: TOKEN_CYBERSOFT,
+  },
+});
+
+requestApi.interceptors.request.use((config) => {
+  let accessToken = null;
+  const state = store.getState();
+
+  if (state.userReducer.userInfo) {
+    accessToken = state.userReducer.userInfo.accessToken;
+
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+export { requestApi };
