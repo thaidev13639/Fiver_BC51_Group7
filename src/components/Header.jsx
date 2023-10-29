c87116ca7b324c13b420253aae81028e705c59d9
thaidev13639 <pnt0964413557@gmail.com>
2023-10-29 23:36:25 +0700

    update responsive header, add service

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ export default function Header() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between navbar-page-home">
                 <button className="navbar-toggler" type="button" >
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className='left-header-home d-flex align-items-center'>
-                    <a className="navbar-brand name-home d-flex mr-5" href="#"><span className='mr-1'>Fiverr</span> <ThunderboltFilled className='logo-home' /></a>
+                    <a className="navbar-brand name-home d-flex mr-3" href="#"><span className='mr-1'>Fiverr</span> <ThunderboltFilled className='logo-home' /></a>
                     <form className="form-inline my-lg-0">
                         <input className="form-control " type="search" placeholder="Find Service" aria-label="Search" />
                         <button className=" my-sm-0 btn-search-home" type="submit">Search</button>
@@ -19,19 +19,19 @@ export default function Header() {
                 </div>
                 <div className='right-header-home d-flex align-items-center'>
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
+                        <li className="nav-item li-1 active">
                             <a className="nav-link" href="#">Fiverr Business<span className="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item li-1">
                             <a className="nav-link" href="#">Explore</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item li-1">
                             <a className="nav-link d-flex align-items-center" href="#"> <GlobalOutlined /><span className='ml-1'>English</span></a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item li-1">
                             <a className="nav-link" href="#">US$ USD</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item li-1">
                             <a className="nav-link" href="#">Become a Seller</a>
                         </li>
                         <li className="nav-item">
@@ -40,7 +40,9 @@ export default function Header() {
                     </ul>
                     <button className="btn btn-outline-success my-sm-0 btn-join-home" type="submit">Join</button>
                 </div>
-                {/* <div className="collapse navbar-collapse " id="navbarTogglerDemo01"></div> */}
+                {/* <div className="collapse navbar-collapse " id="navbarTogglerDemo01">
+                    tesst
+                </div> */}
             </nav>
             <div className='content-nav-home '>
                 <ul className='listContent-nav-home'>
